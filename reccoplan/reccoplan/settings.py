8c6fb7055d5e48a62374fa4ba9e2e134a11720d9
jdengoh <spect13317@gmail.com>
2024-03-15 01:09:43 +0800

    added google key variable as an env variable

--- reccoplan/reccoplan/settings.py
+++ reccoplan/reccoplan/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,6 +105,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
+
+# -- Google API Set-up --
+
+GOOGLE_API_KEY = os.environ.get('GOOGLE_KEY')  # create your local environment variable to connect
+
 
 LANGUAGE_CODE = 'en-us'
 
