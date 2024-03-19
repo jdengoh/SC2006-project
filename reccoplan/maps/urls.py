3c37ab00bc4da620a41fefd1c51726d17445e901
jdengoh <spect13317@gmail.com>
2024-03-19 12:30:06 +0800

    updating html to display single location data

--- reccoplan/maps/urls.py
+++ reccoplan/maps/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'maps-home')
+    path('', views.home, name = 'maps-home'),
+    path('locations/', views.locations, name = 'maps-locations')
 ]
--- reccoplan/maps/views.py
+++ reccoplan/maps/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from .models import *
 
 # -- test function --
 
 def home(request):
     return render(request, 'maps/home.html')
+
+
+class LocationView(View):
+    template_name = "maps/templates/maps/list.html"
+    
+    def get(self.request,pri_key):
+        location = Location.objects.get(pk = pri_key)
+
+        context = {
+            "location":location
+        }
+
+        return render(request, self.template_name, context)
