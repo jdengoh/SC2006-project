fa56f024b287e6f4a9fd1d833baf402a2e406b15
NW <104151966+codesket@users.noreply.github.com>
2024-03-23 03:10:49 +0800

    Add files via upload

--- reccoplan/users/admin.py
+++ reccoplan/users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from . models import UserProfile, UserToken
 
-# Register your models here.
+
+# DOCS - https://docs.djangoproject.com/en/3.1/ref/contrib/admin/
+
+class UserProfileAdmin(admin.ModelAdmin):
+	
+	list_display = ('id', 'user', 'timestamp')
+
+admin.site.register(UserProfile,UserProfileAdmin)
+
+
+class UserTokenAdmin(admin.ModelAdmin):
+	
+	list_display = ('id', 'user', 'timestamp')
+
+admin.site.register(UserToken,UserTokenAdmin)
+#86425
+
