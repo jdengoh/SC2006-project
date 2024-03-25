1f4e38d085b189c3af1267dd8776b4f8c1b603dc
NW <104151966+codesket@users.noreply.github.com>
2024-03-25 20:27:41 +0800

    Update apps.py

--- reccoplan/members/apps.py
+++ reccoplan/members/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class MembersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'members'
+    
+class UsersConfig(AppConfig):
+    name = 'users'
+
+    def ready(self):
+    	import users.signals
