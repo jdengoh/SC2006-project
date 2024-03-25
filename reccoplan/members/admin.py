94912e6c43081bf1c4ab627871b70ad62da16a88
NW <104151966+codesket@users.noreply.github.com>
2024-03-25 20:27:18 +0800

    Update admin.py

--- reccoplan/members/admin.py
+++ reccoplan/members/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from . models import UserProfile, UserToken
+
+
+# DOCS - https://docs.djangoproject.com/en/3.1/ref/contrib/admin/
+
+class UserProfileAdmin(admin.ModelAdmin):
+	
+	list_display = ('id', 'user', 'timestamp')
+
+admin.site.register(UserProfile,UserProfileAdmin)
+
+
+class UserTokenAdmin(admin.ModelAdmin):
+	
+	list_display = ('id', 'user', 'timestamp')
+
+admin.site.register(UserToken,UserTokenAdmin)
+#86425
