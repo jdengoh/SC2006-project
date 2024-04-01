a1f867b775d70e2086ee1da3363975bf8db9b834
tian0662 <125543354+tian0662@users.noreply.github.com>
2024-04-01 20:20:42 +0800

    Google Maps marker

--- reccoplan/members/urls.py
+++ reccoplan/members/urls.py
@@ -5,7 +5,7 @@ from .views import *
 app_name = 'members'
 
 urlpatterns = [
-    #path('', HomeView.as_view(), name = 'maps-home'),
+    # path('', HomeView.as_view(), name = 'maps-home'),
     path('login', views.loginpage, name ='login'),
     path('logout', views.logout_user, name ='logout'),
     path('register', views.registerpage, name = 'register'),
