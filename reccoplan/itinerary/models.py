a41268ea2102c4a3cc4aa8a1e1a2ff32d14aeceb
jdengoh <spect13317@gmail.com>
2024-04-01 14:53:17 +0800

    itinerary model updated

--- reccoplan/itinerary/models.py
+++ reccoplan/itinerary/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class UserItinerary(models.Model):
+
+    name = models.CharField(max_length = 100)
+    rank = models.IntegerField() # how do we change sequence?
--- reccoplan/itinerary/views.py
+++ reccoplan/itinerary/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from .models import *
+from maps.views import *
 
 # Create your views here.
+
+class ItineraryPage(View):
+    template_name = "itinerary/itineraryPage.html"
+    
