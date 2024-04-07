3160895bb4138492ab03324ac9aa6b43e1b8f35d
kenneth <kenneth004@e.ntu.edu.sg>
2024-04-07 23:39:29 +0800

    fixed views

--- reccoplan/members/views.py
+++ reccoplan/members/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import RegisterUserForm
-from .models import UserProfilefrom 
-django.core.mail import send_mail
+from .models import UserProfile 
+from django.core.mail import send_mail
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.template.loader import render_to_string
