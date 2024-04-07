acb4d8cdf1fa523fbb7d0cf3158a761ef8e9e0ab
Jden <spect13317@gmail.com>
2024-04-08 00:42:25 +0800

    update models

--- reccoplan/itinerary/models.py
+++ reccoplan/itinerary/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class UserItinerary(models.Model):
 
     name = models.CharField(max_length = 100)
-    user = models.CharField(max_length = 20, null = True)    #TO-DO: change to foreign key when members model created
+    userID = models.OneToOneField(User, on_delete=models.CASCADE)
